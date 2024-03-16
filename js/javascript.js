c683abf0a04649f21920a6fbf6dc0539a270e1ec
Eder L. Machado <eder.lourenco.machado@gmail.com>
2024-03-15 21:09:52 -0400

    Accessibility: Scroll Behavior Smooth

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+const currentDate = new Date().getFullYear();
+document.querySelector(".copyright span").textContent = currentDate;
+// deactivate right click
+// document.addEventListener('contextmenu', (event) =>{
+//   event.preventDefault();
+// });
